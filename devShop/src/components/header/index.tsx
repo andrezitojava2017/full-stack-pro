696af933965ff1980f2a4f1361c06d140e02477b
Jederson Andre <andre_sjx@live.com>
2026-01-04 13:35:04 -0300

    feat: add libs react-icons, config layout and created header, and implements route

--- devShop/src/components/header/index.tsx
+++ devShop/src/components/header/index.tsx
@@ -1,8 +1,20 @@
+import { FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Welcome to Our Website</h1>
+    <header className="bg-slate-200 w-full ">
+      <nav className="flex items-center justify-between w-full max-w-5xl h-14 px-5 mx-auto">
+        <Link to={"/"} className="font-bold text-2xl">
+          DEV SHOP
+        </Link>
+        <Link to={"/cart"} className="relative">
+          <FiShoppingCart size={28} color="#121212" />
+          <span className="absolute -right-4 -top-2 bg-blue-400 px-2.5 w-5 h-5- rounded-full flex items-center justify-center text-xs text-white">
+            2
+          </span>
+        </Link>
+      </nav>
     </header>
   );
 };
--- devShop/src/routes/routes.tsx
+++ devShop/src/routes/routes.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router";
 import Home from "../pages/home/Home";
 import Cart from "../pages/cart/Cart";
+import Layout from "../components/layout/Layout";
 
 const RoutesShop = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+
+        <Route path="/cart" element={<Cart />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default RoutesShop;
