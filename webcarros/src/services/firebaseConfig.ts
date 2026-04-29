3f10c4b1fff222710272737b2e8c5a42687f0a9d
Jederson Andre <andre_sjx@live.com>
2026-04-29 11:48:05 -0300

    FIX: correção na configuração do firestore, apikeys

--- webcarros/src/services/firebaseConfig.ts
+++ webcarros/src/services/firebaseConfig.ts
@@ -1,17 +1,28 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+const apiKey= import.meta.env.VITE_FIREBASE_API_KEY;
+const authDomain= import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const projectId= import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const storageBucket= import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId= import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const appId= import.meta.env.VITE_FIREBASE_APP_ID;
 // Your web app's Firebase configuration
+console.log("Firebase Config:", {
+  apiKey,
+  authDomain});
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCmdAAlxW4WxHwbXr3WApo6zredbHzTkcs",
-  authDomain: "webcarros-cf556.firebaseapp.com",
-  projectId: "webcarros-cf556",
-  storageBucket: "webcarros-cf556.firebasestorage.app",
-  messagingSenderId: "231473781156",
-  appId: "1:231473781156:web:4c25197780e1cc89f08931"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId
 };
 
 // Initialize Firebase
